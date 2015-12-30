2af76ac1a9a9a52ec7a03f47b72cb7cafa818cdf
Chris B <1029chris@gmail.com>
2015-12-29 21:52:59 -0800

    changed fullscreen stuff

--- conf.lua
+++ conf.lua
@@ -4,4 +4,6 @@ function love.conf(t)
   t.version = "0.9.2"
   t.modules.joystick = false
   t.window.title = "Don't Get a Virus"
+  t.window.fullscreen = true
+  t.window.fullscreentype = "normal"
 end
