d37cb881d850c5a210ed318f994e8e4780aaec9d
Chris B <1029chris@gmail.com>
2015-12-29 21:52:38 -0800

    Set variables, added escape, added rectangle

--- main.lua
+++ main.lua
@@ -1,9 +1,19 @@
 function love.load()
-
+  system = {}
+  system.width = love.graphics.getWidth()
+  system.height = love.graphics.getHeight()
+  system.mouse = {}
+  system.mouse.x = love.mouse.getX
+  system.mouse.y = love.mouse.getY
+  love.graphics.setBackgroundColor(0, 128, 128)
 end
 function love.update(dt)
-
+  system.mouse.x = love.mouse.getX
+  system.mouse.y = love.mouse.getY
+  if love.keyboard.isDown("escape") == true then
+    love.event.quit()
+  end
 end
 function love.draw()
-
+  love.graphics.rectangle("fill", 0, system.height-26, system.width, 26)
 end
