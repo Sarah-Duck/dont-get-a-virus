dbeb792f201f784ff78e75eb3562922bcbb010c6
Chris B <1029chris@gmail.com>
2015-12-30 17:24:34 -0800

    Updating config to 0.10.0

--- conf.lua
+++ conf.lua
@@ -1,9 +1,9 @@
 function love.conf(t)
   t.window.width = 0
   t.window.height = 0
-  t.version = "0.9.2"
+  t.version = "0.10.0"
   t.modules.joystick = false
   t.window.title = "Don't Get a Virus"
   t.window.fullscreen = true
-  t.window.fullscreentype = "normal"
+  t.window.fullscreentype = "exclusive"
 end
