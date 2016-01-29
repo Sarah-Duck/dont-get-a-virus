5cb535c4040af813d76d0fcab5da325eefa282da
Chris B <1029chris@gmail.com>
2016-01-29 13:43:45 -0800

    Panel can un-minimize windows

--- panel.lua
+++ panel.lua
@@ -17,6 +17,16 @@ function drawStart()
     elseif panel.b[1] == 2 then
       love.graphics.print("Internet", panel.s.x+4+72, panel.s.y+7)
     end
+    if (sys.mouse.p.p == true and sys.mouse.p.x >= panel.s.x+72
+    and sys.mouse.p.x <= (panel.s.x+72)+(panel.s.width*2)
+    and sys.mouse.p.y >= panel.s.y and sys.mouse.p.y <= panel.s.y+panel.s.height) then
+      if win[panel.b[1]].min == false then
+        win[panel.b[1]].layer = 1
+      elseif win[panel.b[1]].min == true then
+        win[panel.b[1]].min = false
+        win[panel.b[1]].layer = 1
+      end
+    end
   end
   love.graphics.setColor(150, 150, 150)
   if panel.b[2] ~= 0 then
@@ -26,6 +36,16 @@ function drawStart()
       love.graphics.print("Chat", panel.s.x+4+72+70+67, panel.s.y+7)
     elseif panel.b[2] == 2 then
       love.graphics.print("Internet", panel.s.x+4+72+70+67, panel.s.y+7)
+    end
+    if (sys.mouse.p.p == true and sys.mouse.p.x >= panel.s.x+72+70+67
+    and sys.mouse.p.x <= (panel.s.x+72+70+67)+(panel.s.width*2)
+    and sys.mouse.p.y >= panel.s.y and sys.mouse.p.y <= panel.s.y+panel.s.height) then
+      if win[panel.b[2]].min == false then
+        win[panel.b[2]].layer = 1
+      elseif win[panel.b[2]].min == true then
+        win[panel.b[2]].min = false
+        win[panel.b[2]].layer = 1
+      end
     end
   end
 end
