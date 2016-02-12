c1cf532aac0bff00fa966c126658ff170c44f936
Chris B <1029chris@Chriss-MacBook-Pro.local>
2016-02-12 14:45:38 -0800

    Added desktop drag to highlight thingy

--- desktop.lua
+++ desktop.lua
@@ -1,4 +1,9 @@
 function drawDesktop()
   love.graphics.setColor(255,255,255)
   love.graphics.draw(desktop.bg.current)
+  if sys.mouse.p.p == true and sys.mouse.drag == false then
+    love.graphics.setColor(0,80,160,80)
+    love.graphics.rectangle("fill", sys.mouse.p.x, sys.mouse.p.y, sys.mouse.x-sys.mouse.p.x, sys.mouse.y-sys.mouse.p.y)
+    love.graphics.rectangle("line", sys.mouse.p.x, sys.mouse.p.y, sys.mouse.x-sys.mouse.p.x, sys.mouse.y-sys.mouse.p.y)
+  end
 end
