a59725ac5aad95aab1b000e9b2caf701eb30134d
1029chris <1029chris@gmail.com>
2016-02-16 13:12:13 -0800

    Added third panel

--- panel.lua
+++ panel.lua
@@ -13,9 +13,11 @@ function drawStart()
     love.graphics.rectangle("line", panel.s.x+72, panel.s.y, panel.s.width*2, panel.s.height)
     love.graphics.setColor(70,70,70)
     if panel.b[1] == 1 then
-      love.graphics.print("Chat", panel.s.x+4+72, panel.s.y+7)
+      love.graphics.print(win[1].title, panel.s.x+4+72, panel.s.y+7)
     elseif panel.b[1] == 2 then
-      love.graphics.print("Internet", panel.s.x+4+72, panel.s.y+7)
+      love.graphics.print(win[2].title, panel.s.x+4+72, panel.s.y+7)
+    elseif panel.b[1] == 3 then
+      love.graphics.print(win[3].title, panel.s.x+4+72, panel.s.y+7)
     end
     if (sys.mouse.p.p == true and sys.mouse.p.x >= panel.s.x+72
     and sys.mouse.p.x <= (panel.s.x+72)+(panel.s.width*2)
@@ -36,6 +38,8 @@ function drawStart()
       love.graphics.print("Chat", panel.s.x+4+72+70+67, panel.s.y+7)
     elseif panel.b[2] == 2 then
       love.graphics.print("Internet", panel.s.x+4+72+70+67, panel.s.y+7)
+    elseif panel.b[2] == 3 then
+      love.graphics.print("Files", panel.s.x+4+72+137, panel.s.y+7)
     end
     if (sys.mouse.p.p == true and sys.mouse.p.x >= panel.s.x+72+70+67
     and sys.mouse.p.x <= (panel.s.x+72+70+67)+(panel.s.width*2)
@@ -45,6 +49,28 @@ function drawStart()
       elseif win[panel.b[2]].min == true then
         win[panel.b[2]].min = false
         win[panel.b[2]].layer = 1
+      end
+    end
+  end
+  love.graphics.setColor(150, 150, 150)
+  if panel.b[3] ~= 0 then
+    love.graphics.rectangle("line", panel.s.x+72+137+137, panel.s.y, panel.s.width*2, panel.s.height)
+    love.graphics.setColor(70,70,70)
+    if panel.b[3] == 1 then
+      love.graphics.print("Chat", panel.s.x+4+72+137+137, panel.s.y+7)
+    elseif panel.b[3] == 2 then
+      love.graphics.print("Internet", panel.s.x+4+72+137+137, panel.s.y+7)
+    elseif panel.b[3] == 3 then
+      love.graphics.print("Files", panel.s.x+4+72+137+137, panel.s.y+7)
+    end
+    if (sys.mouse.p.p == true and sys.mouse.p.x >= panel.s.x+72+70+67+137
+    and sys.mouse.p.x <= (panel.s.x+72+137+137)+(panel.s.width*2)
+    and sys.mouse.p.y >= panel.s.y and sys.mouse.p.y <= panel.s.y+panel.s.height) then
+      if win[panel.b[3]].min == false then
+        win[panel.b[3]].layer = 1
+      elseif win[panel.b[2]].min == true then
+        win[panel.b[3]].min = false
+        win[panel.b[3]].layer = 1
       end
     end
   end
