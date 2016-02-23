0ab2713b54e970e1a786b1288cf57b6272d72197
1029chris <1029chris@gmail.com>
2016-02-22 19:34:25 -0800

    Fullscreen mode is default

--- conf.lua
+++ conf.lua
@@ -4,6 +4,6 @@ function love.conf(t)
   t.version = "0.10.0"
   t.modules.joystick = false
   t.window.title = "Don't Get a Virus"
-  t.window.fullscreentype = "exclusive"
-  t.window.fullscreen = false
+  t.window.fullscreentype = "desktop"
+  t.window.fullscreen = true
 end
