8d272c73bb5a70443142465f3c51d68bac89ff27
1029chris <1029chris@gmail.com>
2016-02-25 14:14:15 -0800

    Added music to code

--- assets.lua
+++ assets.lua
@@ -17,9 +17,16 @@ function loadAssets()
     flower = love.graphics.newImage("assets/bg_flower.jpg"),
     city = love.graphics.newImage("assets/bg_city.jpg")
   }
-  music = {}
+  music = {
+    battle1 = love.audio.newSource("assets/music/Thrills And Spills.ogg"),
+    battle2 = love.audio.newSource("assets/music/Less Than Three.ogg"),
+    battle3 = love.audio.newSource("assets/music/Alphys Takes Action.ogg")
+  }
   sound = {
     boot = love.audio.newSource("assets/boot.ogg")
   }
   sound.boot:setLooping(false)
+  music.battle1:setLooping(true)
+  music.battle2:setLooping(true)
+  music.battle3:setLooping(true)
 end
