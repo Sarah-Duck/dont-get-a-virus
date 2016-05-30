fdc485028deac070d2bebbf60880aa3d9ef877b3
1029chris <1029chris@gmail.com>
2016-05-30 11:17:44 -0700

    Added more dialogue for virus

--- dialogue.lua
+++ dialogue.lua
@@ -51,17 +51,39 @@ function loadDia()
   v1.msgs[5] = "Don't even try fighting back! I'm the strongest virus out there!"
   v1.msgs[6] = "That's right! A VIRUS! A program created specifically to exploit MORONS like you!"
   v1.msgs[7] = "And you're my next target! You're dead, kiddo!"
-  v1.msgs[9] = "... Huh?"
-  v1.msgs[10] = "An Antivirus? Maybe you aren't so stupid afterall!"
-  v1.msgs[11] = "But you're still pretty stupid."
-  v1.msgs[13] = "HAHA! PATHETIC! Is that it? Is that all you got?"
-  v1.msgs[14] = "I bet that thing couldn't put a dent in me!"
-  v1.msgs[15] = "Go ahead! Try it, you won't stop me! Position it below me, and press the fire button! Go on! DO IT!"
+  if virus1Lose == 0 then
+    v1.msgs[9] = "... Huh?"
+    v1.msgs[10] = "An Antivirus? Maybe you aren't so stupid afterall!"
+    v1.msgs[11] = "But you're still pretty stupid."
+    v1.msgs[13] = "HAHA! PATHETIC! Is that it? Is that all you got?"
+    v1.msgs[14] = "I bet that thing couldn't put a dent in me!"
+    v1.msgs[15] = "Go ahead! Try it, you won't stop me! Position it below me, and press the fire button! Go on! DO IT!"
+  elseif virus1Lose ~= 0 then
+    v1.msgs[9] = "Oh nooo, you're antiviurs appeared! I've neeeevver seen THAT before!"
+    if virus1Lose == 1 then
+      v1.msgs[10] = "Oh jee whizz, what'll I do? it's not like I BEAT YOU last time!"
+      v1.msgs[11] = "Now it transforms, right? Something like that..."
+    else
+      v1.msgs[10] = "I've beaten you more than once, I ain't scared of that thing!"
+      v1.msgs[11] = "It transforms now, get on with it."
+    end
+    v1.msgs[13] = "Still as pathetic as ever. I beat that little toy last time, what are you doing?"
+    v1.msgs[14] = "It still won't put a dent in me."
+    v1.msgs[15] = "Just shoot me! It still won't even hurt me!"
+  end
   v1.msgs[17] = "Wow, you really are an idiot, huh? Aim it below me and SHOOT IT!"
   v1.msgs[19] = "ARE YOU REALLY THAT STUPID!? SHOOT ME! DO IT YOU MORON!"
   v1.msgs[21] = "WHAT IS WRONG WITH YOU?! ARE YOU SERIOUS!? OR ARE YOU JUST TRYING TO PISS ME OFF!?"
-  v1.msgs[23] = "Whatever. You had your chance! Now it's time to destory you!"
-  v1.msgs[25] = "Ouch! It's stronger than I thought! No matter, because I have defenses of my own!"
+  if virus1Pissed == false then
+    v1.msgs[23] = "Whatever. You had your chance! Now it's time to destory you!"
+  else
+    v1.msgs[23] = "I already played you're little games last time, and I won't fall for that again."
+  end
+  if virus1Lose == 0 then
+    v1.msgs[25] = "Ouch! It's stronger than I thought! No matter, because I have defenses of my own!"
+  elseif virus1Lose ~= 0 then
+    v1.msgs[25] = "I expected that. it hasn't changed since last time! Anyways, now for my part."
+  end
   v1.msgs[26] = "Prepare youself for my UNIMAGINABLY DESTRUCTIVE POWER!"
   v1.msgs[27] = "Also, my giant battleship with multiple turrets."
   v1.msgs[30] = "What do you think? I made it myself! It's made from the numerous computers I have DESTORYED!"
@@ -72,19 +94,27 @@ function loadDia()
   v1.msgs[35] = "Now, it's time to get wriggity wriggity rekt son!"
   v1.msgs[37] = "NO! MY SHIP! I WORKED REALLY HARD ON THAT! IT'LL TAKE WEEKS TO GET ALL THE PARTS AGAIN!"
   v1.msgs[39] = "I WILL STILL DESTROY YOU! We'll just have to do it the old fasioned way!"
-  v1.msgs[41] = "AHH! WHAT!? NO!!!"
-  v1.msgs[42] = "UGH, HOW!? HOW DID YOU BEAT ME!?!? NOBODY IS STRONGER THAN ME!!!"
-  v1.msgs[43] = "OH GOD IT HURTS! OW OW OW!!! YOU JUST GOT LUCKY!!! NO, YOU CHEATED!!! CHEATER!!!"
-  v1.msgs[44] = "WHATEVER YOU DID, IT ISN'T FAIR!!! YOU DICKHEAD!!! AAAHH!!!"
-  v1.msgs[45] = "THIS ISN'T OVER!!! I WILL STILL DESTORY YOU!!! ONE DAY!!! WATCH YOUR BACK!!!"
+  if virus1Lose == 0 then
+    v1.msgs[41] = "AHH! WHAT!? NO!!!"
+    v1.msgs[42] = "UGH, HOW!? HOW DID YOU BEAT ME!?!? NOBODY IS STRONGER THAN ME!!!"
+    v1.msgs[43] = "OH GOD IT HURTS! OW OW OW!!! YOU JUST GOT LUCKY!!! NO, YOU CHEATED!!! CHEATER!!!"
+    v1.msgs[44] = "WHATEVER YOU DID, IT ISN'T FAIR!!! YOU DICKHEAD!!! AAAHH!!!"
+    v1.msgs[45] = "THIS ISN'T OVER!!! I WILL STILL DESTORY YOU!!! ONE DAY!!! WATCH YOUR BACK!!!"
+  elseif virus1Lose ~= 0 then
+    v1.msgs[41] = "AAAH! NO! WHAT? HOW!?"
+    v1.msgs[42] = "I BEAT YOU LAST TIME! HOW DID YOU DO THIS!? THIS ISN'T POSSIBLE!"
+    v1.msgs[43] = "YOU CHEATED, DIDN'T YOU!? HOW DID YOU COME BACK AFTER I BEAT YOU!?"
+    v1.msgs[44] = "HOW DID YOU RESTART EVERYTHING FROM THE BEGGINING!? HOW!?"
+    v1.msgs[45] = "THAT SHOULDN'T BE POSSIBLE! YOU CHEATED! YOU'RE A HACKER! YOU FUCKING ASSHOLE!"
+  end
   v1.msgs[46] = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH!!!!!!!!!!!!!!!!!!!!!!"
   if virus1Lose <= 1 then
     v1.msgs[50] = "AAAAHAHAHAH! I WIN YET AGAIN! ANOTHER COMPUTER, DOMINATED BY ME!!! AAAAAHAHAHA!!!"
     v1.msgs[51] = "I WIN! YOU LOSE! JUST LIKE IT'S MEANT TO BE! JUST LIKE IT ALWAYS WILL BE!"
     v1.msgs[52] = "AND FOR THE GRAND FINALE, YOUR COMPUTER IS DESTORYED! HAHAHA!!!"
   elseif virus1Lose >= 2 then
-    v1.msgs[50] = "Yaddah Yaddah Yaddah, I win..."
-    v1.msgs[51] = "Blah blah blah, you lose."
-    v1.msgs[52] = "Now fuck off!"
+    v1.msgs[50] = "Yaddah Yaddah Yaddah, I win... Blah blah blah, you lose."
+    v1.msgs[51] = "Seriously! I won! Stop being such a sore loser by reseting the system!"
+    v1.msgs[52] = "Just fuck off!"
   end
 end
