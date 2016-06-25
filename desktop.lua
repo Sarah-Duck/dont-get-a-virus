13de07d1a55433db2346428dced9c8932c89a3d4
1029chris <1029chris@gmail.com>
2016-06-25 14:52:00 -0700

    Tentacles shake with the virus

--- desktop.lua
+++ desktop.lua
@@ -135,15 +135,15 @@ function drawTentacles()
     end
     if v1.t.t[i].si == 0 then
       if v1.t.t[i].t == 1 then
-        love.graphics.draw(v1.tent1,v1.t.t[i].x+v1.t.x,v1.t.t[i].y,math.rad(-90+v1.t.t[i].r),v1.t.t[i].s*sys.sh,v1.t.t[i].sy*sys.sw+v1.t.t[i].syc,16)
+        love.graphics.draw(v1.tent1,v1.t.t[i].x+v1.t.x+v1.shakex,v1.t.t[i].y+v1.shakey,math.rad(-90+v1.t.t[i].r),v1.t.t[i].s*sys.sh,v1.t.t[i].sy*sys.sw+v1.t.t[i].syc,16)
       elseif v1.t.t[i].t == 2 then
-        love.graphics.draw(v1.tent2,v1.t.t[i].x+v1.t.x,v1.t.t[i].y,math.rad(-90+v1.t.t[i].r),v1.t.t[i].s*sys.sh,v1.t.t[i].sy*sys.sw+v1.t.t[i].syc,16)
+        love.graphics.draw(v1.tent2,v1.t.t[i].x+v1.t.x+v1.shakex,v1.t.t[i].y+v1.shakey,math.rad(-90+v1.t.t[i].r),v1.t.t[i].s*sys.sh,v1.t.t[i].sy*sys.sw+v1.t.t[i].syc,16)
       end
     elseif v1.t.t[i].si == 1 then
       if v1.t.t[i].t == 1 then
-        love.graphics.draw(v1.tent1,(-v1.t.t[i].x)+(sys.w-v1.t.x),v1.t.t[i].y,math.rad(90+v1.t.t[i].r),v1.t.t[i].s*sys.sh,v1.t.t[i].sy*sys.sw+v1.t.t[i].syc,16)
+        love.graphics.draw(v1.tent1,(-v1.t.t[i].x)+(sys.w-v1.t.x)+v1.shakex,v1.t.t[i].y+v1.shakey,math.rad(90+v1.t.t[i].r),v1.t.t[i].s*sys.sh,v1.t.t[i].sy*sys.sw+v1.t.t[i].syc,16)
       elseif v1.t.t[i].t == 2 then
-        love.graphics.draw(v1.tent2,(-v1.t.t[i].x)+(sys.w-v1.t.x),v1.t.t[i].y,math.rad(90+v1.t.t[i].r),v1.t.t[i].s*sys.sh,v1.t.t[i].sy*sys.sw+v1.t.t[i].syc,16)
+        love.graphics.draw(v1.tent2,(-v1.t.t[i].x)+(sys.w-v1.t.x)+v1.shakex,v1.t.t[i].y+v1.shakey,math.rad(90+v1.t.t[i].r),v1.t.t[i].s*sys.sh,v1.t.t[i].sy*sys.sw+v1.t.t[i].syc,16)
       end
     end
     if v1.t.t[i].d == false and v1.c.chat.msgs <= 40 then
